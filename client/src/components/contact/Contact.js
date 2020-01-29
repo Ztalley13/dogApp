1e711339dbb9deb54220bf73e1afdecb121b73db
Zack White <zrwhite2993@gmail.com>
2020-01-29 10:21:16 -0600

    Completed contact from shell

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -1,20 +1,67 @@
 import React from 'react';
-//import './styles.css';
+import './style.css';
 
 function ContactPage() {
   return (
     <body>
       <section class="hero is-info">
         <div class="hero-body">
-          <div class="container">
-            <h1 class="title">
-              {"Kibbles & Brews"}
-            </h1>
-            <h2 class="subtitle">
-              Contact Us!
-            </h2>
-          </div>
+        <div class="container">
+          <h1 class="title">
+            {"Kibbles and Brews"}
+          </h1>
+          <h2 class="subtitle">
+            {"Contact Us"}
+          </h2>
         </div>
+        </div>
+      </section>
+
+      <section class="contact-input">
+      <div class="field">
+        <label class="label">Name</label>
+      <div class="control">
+        <input class="input" type="text"/>
+      </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Username</label>
+      <div class="control has-icons-left has-icons-right">
+        <input class="input is-success" type="text"/>
+        
+        <span class="icon is-small is-left">
+          <i class="fas fa-user"></i>
+        </span>
+      </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Email</label>
+      <div class="control has-icons-left has-icons-right">
+        <input class="input is-danger" type="email"/>
+        <span class="icon is-small is-left">
+          <i class="fas fa-envelope"></i>
+        </span>
+      </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Message</label>
+      <div class="control">
+        <textarea class="textarea" placeholder="Textarea"></textarea>
+      </div>
+      </div>
+
+      <div class="field is-grouped">
+        <div class="control">
+          <button class="button is-link">Submit</button>
+        </div>
+        <div class="control">
+          <button class="button is-link is-light">Cancel</button>
+        </div>
+      </div>
+    
       </section>
     </body>
   )
