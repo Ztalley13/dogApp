1d363d2d8ee18689c0d3fa71bf253e9159951f3d
Austin Williams <auswil13@gmail.com>
2020-01-31 14:48:15 -0600

    another attempt at fixing routes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const path = require('path');
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
@@ -12,12 +12,12 @@ app.use(express.json());
 app.use(express.static("public"));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname,"client/build")));
+  app.use(express.static(path.join(__dirname + "client/build")));
 }
 
 app.post("/members", function(req, res) {
-    res.sendStatus(200);
-  })
+  res.sendStatus(200);
+});
 // Add routes, both API and view
 app.use(routes);
 
