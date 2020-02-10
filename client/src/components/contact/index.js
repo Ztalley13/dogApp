b56a1024d8e842528e690fe69ae08b9453aac070
Austin Williams <auswil13@gmail.com>
2020-02-10 15:51:16 -0600

    over it

--- client/src/components/contact/index.js
+++ client/src/components/contact/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "./node_modules/react";
 // import { useToasts } from "react-toast-notifications";
 import './style.css';
 
--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import NavBar from "../components/NavBar";
-import ContactPage from "../components/Contact";
+import ContactPage from "../components/contact";
 
 
 
